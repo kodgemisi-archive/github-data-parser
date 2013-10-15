df4ea2659954b95ef66991ff6591fb533a0de5b4
Mehmet Beydogan <mbbrain@gmail.com>
2013-10-15 17:05:12 +0300

    -github_api initilization is done. -Configuration module is removed.

--- lib/github_data_parser.rb
+++ lib/github_data_parser.rb
@@ -1,16 +1,27 @@
 require "github_data_parser/version"
-require "github_data_parser/configuration"
 require 'github_api'
 
 module GithubDataParser
-  extend Configuration
 
   class << self
 
-    attr_accessor :client_id,
+    attr_accessor :client_id
+    attr_accessor :client_secret
+    attr_accessor :github_api
 
-    def new(options = {}, &block)
-      Github::Client.new(options, &block)
+    def new(options = {})
+      self.client_id = options[:client_id]
+      self.client_secret = options[:client_secret]
+
+      self.github_api = Github.new do |config|
+        config.client_id = self.client_id
+        config.client_secret = self.client_secret
+        config.per_page = 100
+      end
+
+
+      puts github_api.repos.list user: 'beydogan'
+
     end
 
   end
